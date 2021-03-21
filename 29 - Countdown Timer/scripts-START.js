7c1b2327bee4775056c3ed1dff16ed9c67369d43
tomasep22 <tomas.ep22@gmail.com>
2021-03-21 16:16:06 -0300

    comment 29 and 30

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,59 +1,130 @@
 const timerdiv = document.querySelector('.timer');
-const controls = timerdiv.querySelector('.timer__controls');
 const buttons = timerdiv.querySelectorAll('[data-time]');
 const timerDisplay = timerdiv.querySelector('.display__time-left')
 const endTime = timerdiv.querySelector('.display__end-time');
-console.log(timerdiv, controls, buttons);
 let countdown;
 
-
 function timer(seconds) {
-clearInterval(countdown);
- const now = Date.now();
- const then = now + seconds * 1000
- displayTimeLeft(seconds);
- displayEndTime(then);
- 
- countdown = setInterval(() => {
-    const secondsLeft = Math.round((then - Date.now()) / 1000);
-    console.log(secondsLeft)
-    if(secondsLeft < 0) {
-        clearInterval(countdown);
-        return;
-    }
-    displayTimeLeft(secondsLeft);
- }, 1000);
+    clearInterval(countdown);
+    const now = Date.now();
+    const then = now + seconds * 1000;
+    displayEndTime(then);
+    displayTimeLeft(seconds)
+    countdown = setInterval(() => {
+        let secondsLeft = Math.round((then - Date.now()) / 1000);
+        displayTimeLeft(secondsLeft);
+        console.log(secondsLeft);
+        if(secondsLeft <= 0) {
+            clearInterval(countdown);
+            return;
+        };
+    }, 1000);
 };
 
 function displayTimeLeft(seconds) {
     const minutes = Math.floor(seconds / 60);
     const remainderSeconds = seconds % 60;
-    const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
-    document.title = display;
+    const display = `${minutes}:${remainderSeconds < 10 ? '0' + remainderSeconds : remainderSeconds}`;
     timerDisplay.textContent = display;
-}
+    document.title = display;  
+};
 
-function displayEndTime(timesStamp) {
- const end = new Date(timesStamp)
- const hour = end.getHours();
- const minutes = end.getMinutes();
- const adjustedHour = hour > 12 ? hour -12 : hour;
- endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}}`;
-
-
-}
+function displayEndTime(timestamp) {
+    const end = new Date(timestamp);
+    const minutes = end.getMinutes();
+    const hours = end.getHours();
+    endTime.textContent = `be back at: ${hours - 12}:${minutes < 10 ? '0' + minutes : minutes}`;
+};
 
 function startTimer() {
-    const seconds = parseInt(this.dataset.time);
-    timer(seconds);
+const seconds = parseInt(this.dataset.time);
+timer(seconds);
 }
 
 buttons.forEach(button => button.addEventListener('click', startTimer));
 
 document.customForm.addEventListener('submit', function(e) {
-    e.preventDefault();
-    const mins = this.minutes.value;
-    console.log(mins);
-    timer(mins * 60);
-    this.reset();
-})
+    e.preventDefault()
+    const minutes = document.customForm.minutes.value
+    this.reset()
+    timer(minutes * 60);
+});
+
+
+//  1 function timer(seconds)
+/* 
+    Guardamos en una variable el tiempo en el momento que
+corre la funcion (el inicio).
+    Otra variable para guardar cuando termina el timer.
+    Usamos un interval que corra cada un segundo,
+ guardando en una variable cunatos segundos quedan entre
+ el presente y el final.
+    Si no quedan segundos de diferencia entre el presente
+y el final el timer se debe parar
+    Si quedan segundos hay que actualizar el display
+mostrando cuantos segundos quedan (otra funcion). los
+segundos deben ser numeros enteros.
+    El interval no arranca de inmediato, tarda 1s, la
+consecuencia es que cuando corremos displayTimeLeft() en
+el timer ya paso un segundo, por lo tanto no muestra el
+primer segundo. Hay que arreglar eso.
+*/
+
+// 2 function displayTimeLeft(seconds)
+/*
+    Guardamos los segundos en minutos sin segundos. por ej
+si quedan 2 minutos 36 segundos guardamos 2.
+    Guardamos los segundos que despreciamos en la variable
+de arriba en otra variable.
+    Guardamos en una variable lo que vamos a mostrar en el
+display. queremos un formato del estilo 11:11.
+    Si quedan < 10s el dispay debe ser 1:06 y no 1:6.
+    Mostamos en el display y en el titulo de la pestaña
+los segundo que quedan.
+*/
+
+// 3 function displayEndTime(timestamp)
+/*
+    Esta funcion va a que hora termina la cuenta atras.
+    Primero usamos el timestamp para crear un objeto date
+con el valor de a que momento termina.
+    El objeto creado con date tiene mucha informacion,
+vamos a tomar la hora y los minutos.
+    Cambiamos el texto del display end time a 'be back at
+"la hora y minutos que termina"'.
+    Esta funcion debe correr en la primer funcion.
+    Queremos que el display tenga un formato 0-12 y no 12-24
+para la hora(No hace falta aclarar AM / PM).
+    Si los minutos < 10 quremos que se vea 3:00 y no 3:0.
+*/
+
+// 4 function startTimer()
+/* 
+    Esta funcion corre cuando el usuario toca alguno de
+los botones con [data-time].
+    pasamos la info del data-time a un numero 
+(no usa Number()). y la guardamos en una variable.
+despues corremos la primera funcion pasando esa variable
+    Hay que modificar la primer funcion para que si toco dos
+botones se cancele el interval del primer click.
+*/
+
+// 5 function(e)
+/* 
+    Queremos una funcion que haga correr el timer en base
+al submit del usuario en el form.
+    le agregamos un eventListener al form, el hace esto
+sin seleccionar el objeto (lo hace asi no pq haya que
+hacerlo así, sino para mostrar otra manera de hacerlo). 
+    Esto es posible porque los elementos form y input tienen
+ un atributo name.
+
+    Cuando corre la funcion la pagina no se debe actualizar.
+    Guardamos los minutos que hace input el usuario
+    en una variable.
+    Queremos que el input vuelva a su estado original 
+despues de haber guardado la informacion en la variable
+(no es un if).
+    Pasamos la primera funcion pasando los minutos * 60
+    (pq la primera funcion toma segundos)
+*/
